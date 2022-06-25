510d6b0e650ddb2cd2433848800c2be165f27c8d
westbro <westbro00@gmail.com>
2022-06-26 02:13:32 +0900

    feat: 배경화면 클릭 시 변경하도록 적용

--- src/App.svelte
+++ src/App.svelte
@@ -3,25 +3,25 @@
 	import { onMount } from "svelte";
 
 	onMount(async () => {
+		setBackgrounImage();
+	});
+
+	document.body.addEventListener('click', setBackgrounImage, true); 
+
+	function setBackgrounImage() {
 		fetch("https://api.unsplash.com/photos/random?client_id=3nqwVq8pU9DMo1p0-W_6jrMnawq84xoo3RGuemcMByA")
 		.then(response => response.json())
 		.then(data => {
-			setBackgrounImage(data.urls.regular);
+			Object.assign(document.body.style, {
+				backgroundColor: 'darkgray',
+				backgroundImage: `url(${data.urls.regular})`,
+				backgroundSize: 'cover'
+			})
 		}).catch(error => {
 			console.log(error);
 			return [];
 		});
-	});
-
-	function setBackgrounImage(src) {
-		console.log('src', src)
-		Object.assign(document.body.style, {
-			backgroundColor: 'darkgray',
-			backgroundImage: `url(${src})`,
-			backgroundSize: 'cover'
-		})
 	}
-
 </script>
 
 <Header />
