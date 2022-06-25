6541e1fea6d8b530b10318efbe5580b436f2153e
westbro <westbro00@gmail.com>
2022-06-26 00:19:54 +0900

    feat: Header 컴포넌트 생성 및 trello 로고 추가

---
+++ src/components/Header.svelte
@@ -0,0 +1,19 @@
+<header>
+    <img src="/images/trello-logo.svg" alt="Trello logo" class="logo">
+</header>
+
+<style>
+    header {
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: rgba(0, 0, 0, 0.3);
+        /* border: 1px salmon solid; */
+    }
+
+    header img.logo {
+        width: 80px;
+        opacity: 0.5;
+    }
+</style>
