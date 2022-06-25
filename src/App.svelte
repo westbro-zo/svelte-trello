68ef7ee7a22704f77ebea1fe05a10e894388521e
westbro <westbro00@gmail.com>
2022-06-26 01:53:34 +0900

    feat: Unspash api 연동 및 받아온 랜덤 이미지 배경화면에 적용

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,27 @@
 <script>
-	import Header from './components/Header.svelte';
+	import Header from '~/components/Header.svelte';
+	import { onMount } from "svelte";
+
+	onMount(async () => {
+		fetch("https://api.unsplash.com/photos/random?client_id=3nqwVq8pU9DMo1p0-W_6jrMnawq84xoo3RGuemcMByA")
+		.then(response => response.json())
+		.then(data => {
+			setBackgrounImage(data.urls.regular);
+		}).catch(error => {
+			console.log(error);
+			return [];
+		});
+	});
+
+	function setBackgrounImage(src) {
+		console.log('src', src)
+		Object.assign(document.body.style, {
+			backgroundColor: 'darkgray',
+			backgroundImage: `url(${src})`,
+			backgroundSize: 'cover'
+		})
+	}
+
 </script>
 
 <Header />
