b603491c2523ab0cee6d223539fa334dff435c19
westbro <westbro00@gmail.com>
2022-06-27 01:41:34 +0900

    feat: 리스트, 리스트 타이틀, 카드, 카드생성 컴포넌트 추가

--- src/components/List.svelte
+++ src/components/List.svelte
@@ -1,9 +1,24 @@
 <script>
+    import CreateCard from '~/components/CreateCard.svelte';
+    import ListTitle from '~/components/ListTitle.svelte';
+    import Card from '~/components/Card.svelte';
+
     export let list;
 </script>
 
 <div class="list">
-    {list.title}
+    <div class="list__inner">
+        <div class="list__heading">
+            <ListTitle {list} />
+            <p>{list.cards.length} cards</p>
+        </div>
+        <div class="list__cards">
+            {#each list.cards as card (card.id)}
+                <Card />
+            {/each}
+        </div>
+        <CreateCard />
+    </div>
 </div>
 
 <style lang="scss">
@@ -14,5 +29,27 @@
         border: 1px solid peru;
         box-sizing: border-box;
         margin: 0 4px;
+
+        .list__inner {
+            display: flex;
+            flex-direction: column;
+            max-height: 100%;
+            padding: 10px 8px;
+            box-sizing: border-box;
+            background: #ebecf0;
+            border-radius: 4px;
+            .list__heading {
+                margin-bottom: 10px;
+                p {
+                    color: #5e6c84;
+                    padding: 0 8px;
+                }
+                .list__cards {
+                    overflow-x: hidden;
+                    overflow-y: auto;
+                    margin-bottom: 10px;
+                }
+            }
+        }
     }
 </style>
---
+++ src/components/ListTitle.svelte
@@ -0,0 +1,86 @@
+<script>
+    import { tick } from 'svelte';
+    import {lists} from '~/store/list';
+    import {autoFocusout} from '~/actions/autoFocusout';
+
+    export let list;
+    let isEditMode = false;
+    let title = list.title;
+    let textareaEl;
+
+    function saveTitle() {
+        if (title.trim()) {
+            // TODO: 타이틀 수정
+            // lists.edit({
+            //     id: list.id,
+            //     title
+            // })
+        }
+        offEditMode();
+    }
+    function removeTitle() {
+        // TODO: 리스트 제거
+        // lists.remove({
+        //     id: list.id
+        // })
+        offEditMode();
+    }
+    async function onEditMode() {
+        isEditMode = true;
+        title = list.title;
+        await tick();
+        textareaEl && textareaEl.focus();
+    }
+    function offEditMode() {
+        isEditMode = false;
+    }
+</script>
+
+{#if isEditMode}
+    <div
+        use:autoFocusout={offEditMode}
+        class="edit-mode"
+    >
+        <textarea
+            bind:value={title}
+            bind:this={textareaEl}
+            placeholder="Enter a title for this list..."
+            on:keydoen={event => {
+                event.key === 'Enter' && saveTitle()
+                event.key === 'Escape' && offEditMode()
+                event.key === 'Esc' && offEditMode()
+        }}></textarea>
+        <div class="actions">
+            <div class="btn success" on:click={saveTitle}>Save</div>
+            <div class="btn" on:click={offEditMode}>Cancel</div>
+            <div class="btn danger" on:click={removeTitle}>Delete List</div>
+        </div>
+    </div>
+
+{:else}
+    <h2 class="title">
+        {list.title}
+        <div class="btn small edit-btn-for-list" on:click={onEditMode}>
+            Edit
+        </div>
+    </h2>
+{/if}
+
+<style lang="scss">
+    h2.title {
+        position: relative;
+        font-weight: 700;
+        padding: 4px 8px;
+        cursor: pointer;
+
+        .btn {
+            position: absolute;
+            top: 0;
+            right: 0;
+            display: none;
+        }
+    }
+    :global(.list__inner:hover .edit-btn-for-list) {
+        display: block !important;
+    }
+</style>
