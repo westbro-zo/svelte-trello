00d40704877c5649c198cce1c6fc9856228eb4cc
westbro <westbro00@gmail.com>
2022-08-03 05:28:32 +0900

    feat: 헤더 외 화면 클릭시 리스트 생성 팝업 띄우기 & 리스트 생성

---
+++ src/actions/addListWithDblClick.js
@@ -0,0 +1,61 @@
+
+import { get } from 'svelte/store';
+import { lists } from '~/store/list';
+
+export function addListWithDblClick(el) {
+    const handleDblClick = (e) => {
+        e.stopPropagation();
+
+        const HEADER_HEIGHT = 40;
+        const cursorX = e.clientX + el.scrollLeft;
+        const cursorY = e.clientY - HEADER_HEIGHT;
+
+        addElement(cursorX, cursorY);
+    }
+
+    el.addEventListener('dblclick', handleDblClick);
+
+    return {
+        destroy() {
+            el.removeEventListener('dblclick', handleDblClick);
+        }
+    }
+}
+
+function addElement(x, y) {
+    const popupEl = document.getElementById('popup');
+    const positionEl = popupEl.querySelector('.position');
+    const popupTitleEl = popupEl.querySelector('.popup_title');
+
+    popupEl.setAttribute("class", "popup show edit-mode");
+    popupEl.style.cssText = `top:${y}px; left:${x}px;`
+
+    const position = getPosition(x);
+
+    positionEl.value = `${position}`;
+    positionEl.setAttribute('value', position);
+    
+    popupTitleEl.focus();
+}
+
+
+function getPosition(x) {
+    let pos;
+
+    const LIST_WIDTH = 290;
+    const LIST_LEFT_MARGIN = 4;
+    const CONTAINER_LEFT_PADDING = 30;
+    const maxIndex = get(lists).length + 1;
+
+    if (x < CONTAINER_LEFT_PADDING + LIST_LEFT_MARGIN) { // 34
+        pos = 0;
+    } else {
+        let tmp = x - CONTAINER_LEFT_PADDING - LIST_LEFT_MARGIN;
+
+        pos = tmp / (LIST_LEFT_MARGIN * 2 + LIST_WIDTH);
+    }
+
+    pos = Math.floor(pos) + 1;
+
+    return pos > maxIndex ? maxIndex : pos;
+}
--- src/components/ListContainer.svelte
+++ src/components/ListContainer.svelte
@@ -3,16 +3,53 @@
     import CreateList from '~/components/CreateList.svelte';
     import { lists } from '~/store/list';
     import { onMount } from 'svelte';
+    import { addListWithDblClick } from '~/actions/addListWithDblClick';
+    import { v4 as uuidv4 } from 'uuid';
+    import { DEFAULT_POS } from '~/app.value'
+
+    let hidePopup = true;
+    let title = "New List";
+    let positionEl;
+
+    $: sortList = $lists.sort((x, y) => x.pos - y.pos);
 
     onMount(() => {
         lists.reset();
     })
 
+    function cancel() {
+        // FIXME: 수정 필요
+        console.log('cancel');
+        hidePopup = true;
+    }
+
+    function addList() {
+        const index = Number(positionEl.value) - 1;
+        const pos = getNewPos(index, $lists);
+
+        if (title.trim()) {
+            lists.add({
+                id: uuidv4(),
+                title,
+                pos
+            })
+        }
+
+        hidePopup = true;
+    }
+
+    function getNewPos(index, arr) {
+        const prevPos = index === 0 ? 0 : arr[index-1].pos;
+        const nextPos = index === arr.length ? arr[arr.length - 1].pos + DEFAULT_POS : arr[index].pos;
+
+        return (prevPos + nextPos) / 2;
+    }
+
 </script>
 
-<div class="list-container">
+<div class="list-container" use:addListWithDblClick={addList}>
     <div class="lists">
-        {#each $lists as list, index (list.id)}
+        {#each sortList as list, index (list.id)}
             <List
                 {list}
                 listIndex={index}
@@ -20,6 +57,21 @@
         {/each}
     </div>
     <CreateList />
+    <div id="popup" class="popup" class:hide="{hidePopup === true}">
+        <div class="list__heading">Add List</div>
+        <textarea class="popup_title" bind:value={title}></textarea>
+        <div class="actions">
+            <div class="btn success" on:click={addList}>
+                Add card
+            </div>
+            <div class="btn" on:click={cancel}>
+                Cancel
+            </div>
+            <div class="pos__text">
+                position <input bind:this={positionEl} class="position"/>
+            </div>
+        </div>
+    </div>
 </div>
 
 <style lang="scss">
@@ -31,12 +83,45 @@
         overflow-x: auto;
         overflow-y: hidden;
         white-space: nowrap;
+        position: relative;
 
         .lists {
             display: inline-block;
-            height: 100%;
+            // height: 100%;
             box-sizing: border-box;
             white-space: nowrap;
+        }
+    }
+
+    :global(.popup) {
+        position: absolute;
+        width: 290px;
+        height: 100px;
+        display: flex;
+        flex-direction: column;
+        padding: 8px;
+        background: #ebecf0;
+        border-radius: 4px;
+
+        .pos__text {
+            display: inline-block;
+            margin: 0 20px;
+            line-height: 30px;
+            font-size: 14px;
+            font-weight: 400;
+        }
+
+        .position {
+            width: 40px;
+            height: 20px;
+        }
+
+        &.hide {
+            display: none;
+        }
+
+        &.show {
+            display: flex;
         }
     }
 </style>
